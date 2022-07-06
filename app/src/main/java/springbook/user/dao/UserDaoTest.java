b0c914a9a28f2ac1e3b6f30f12e2711b6b60a43c
fosong98 <fosong98@gmail.com>
2022-07-07 00:29:02 +0900

    <test> : Add count test
    * getCount() 함수를 검사하는 좀 더 포괄적인 테스트를 추가

--- app/src/main/java/springbook/user/dao/UserDaoTest.java
+++ app/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,6 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -9,6 +10,7 @@ import java.sql.SQLException;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class UserDaoTest {
@@ -34,5 +36,28 @@ public class UserDaoTest {
 
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext(
+                "applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "park", "springno1");
+        User user2 = new User("leegw700", "Lee", "springno2");
+        User user3 = new User("bumjin", "park2", "springno3");
+
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
+
+        dao.add(user1);
+        assertEquals(dao.getCount(), 1);
+
+        dao.add(user2);
+        assertEquals(dao.getCount(), 2);
+
+        dao.add(user3);
+        assertEquals(dao.getCount(), 3);
     }
 }

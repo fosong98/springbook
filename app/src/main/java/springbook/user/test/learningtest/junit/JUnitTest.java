6b1e2e4dcc45757300a8ae3abc2bcfeef3bf5804
fosong98 <fosong98@gmail.com>
2022-07-08 18:41:59 +0900

    <test> : Add application context code to JUnitTest
    * Application context의 생성을 확인하기 위한 테스트를 추가

--- app/src/main/java/springbook/user/test/learningtest/junit/JUnitTest.java
+++ app/src/main/java/springbook/user/test/learningtest/junit/JUnitTest.java
@@ -2,6 +2,11 @@ package springbook.user.test.learningtest.junit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,24 +14,38 @@ import java.util.Set;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/junit.xml")
 public class JUnitTest {
+    @Autowired
+    ApplicationContext context;
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 }

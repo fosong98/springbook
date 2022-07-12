381d907cfe9f9beb55e2d1316a02f2074e3feb5a
fosong98 <fosong98@gmail.com>
2022-07-12 20:16:46 +0900

    <fix> : Apply try-with-resources in UserDao.deleteAll
    * UserDao.deleteAll 메소드에 예외처리 추가

--- app/src/main/java/springbook/user/dao/UserDao.java
+++ app/src/main/java/springbook/user/dao/UserDao.java
@@ -61,13 +61,12 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        try (Connection c = dataSource.getConnection();
+             PreparedStatement ps = c.prepareStatement("delete from users")) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     public int getCount() throws SQLException {

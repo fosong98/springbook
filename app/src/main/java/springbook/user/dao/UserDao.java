6efb4c69208d36744bf2b748f5906ca8cadf3ab7
fosong98 <79358032+fosong98@users.noreply.github.com>
2022-07-12 15:20:49 +0900

    <fix> : Apply try-with-resources in UserDao.getCount
    * getCount 메소드에 예외 처리 구문 작성

--- app/src/main/java/springbook/user/dao/UserDao.java
+++ app/src/main/java/springbook/user/dao/UserDao.java
@@ -72,18 +72,14 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+        try (
+                Connection c = dataSource.getConnection();
+                PreparedStatement ps = c.prepareStatement("select count(*) from users");
+                ResultSet rs = ps.executeQuery();
+        ) {
+            rs.next();
+            int count = rs.getInt(1);
+            return count;
+        }
     }
 }

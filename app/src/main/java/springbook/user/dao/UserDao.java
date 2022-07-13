93ed6f25777bd544111c57b5e020a384e9a01664
fosong98 <79358032+fosong98@users.noreply.github.com>
2022-07-13 11:01:41 +0900

    <clean> : Apply anonymous inner class to add deleteAll method
    * 익명 내부 클래스를 사용하여 UserDao의 add와 deleteAll 메소드 작성
    * 클래스 수를 줄이고 인스턴스 변수와 생성자를 만들지 않아도 된다.

--- app/src/main/java/springbook/user/dao/UserDao.java
+++ app/src/main/java/springbook/user/dao/UserDao.java
@@ -21,21 +21,20 @@ public class UserDao {
     }
 
     public void add(final User user) throws SQLException {
-        class AddStatement implements StatementStrategy {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        jdbcContextWithStatementStrategy(
+            new StatementStrategy() {
+                @Override
+                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+                    ps.setString(1, user.getId());
+                    ps.setString(2, user.getName());
+                    ps.setString(3, user.getPassword());
 
-                return ps;
+                    return ps;
+                }
             }
-        }
-
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
+        );
     }
 
     public User get(String id) throws SQLException {
@@ -65,8 +64,13 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("delete from users");
+                return ps;
+            }
+        });
     }
 
     public int getCount() throws SQLException {

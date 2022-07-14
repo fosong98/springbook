aa7553d4614f7fcc4bbb6518f09c607eda33e510
fosong98 <fosong98@gmail.com>
2022-07-14 22:13:59 +0900

    <clean> : Add multiply function and Modify to template callback pattern
    * 곱셈 기능을 추가하고 template callback 패턴을 적용

--- app/src/main/java/springbook/user/test/learningtest/template/CalcSumTest.java
+++ app/src/main/java/springbook/user/test/learningtest/template/CalcSumTest.java
@@ -1,17 +1,31 @@
 package springbook.user.test.learningtest.template;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String filePath;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        filePath = new ClassPathResource("/numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(new ClassPathResource("/numbers.txt").getPath());
-        assertEquals(sum, 10);
+        assertEquals(calculator.calcSum(filePath), 10);
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertEquals(calculator.calcMultiply(filePath), 24);
     }
 }
--- app/src/main/java/springbook/user/test/learningtest/template/Calculator.java
+++ app/src/main/java/springbook/user/test/learningtest/template/Calculator.java
@@ -1,20 +1,52 @@
 package springbook.user.test.learningtest.template;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-    public Integer calcSum(String filePath) throws IOException {
+    public int calcSum(String filePath) throws IOException {
+        BufferedReaderCallback sumCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader reader) throws IOException {
+                        Integer sum = 0;
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            sum += Integer.parseInt(line);
+                        }
+                        return sum;
+                    }
+                };
+
+        return fileReaderTemplate(filePath, sumCallback);
+    }
+
+    public int calcMultiply(String filePath) throws IOException {
+        BufferedReaderCallback multiplyCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader reader) throws IOException {
+                        int multiply = 1;
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            multiply *= Integer.valueOf(line);
+                        }
+
+                        return multiply;
+                    }
+                };
+
+        return fileReaderTemplate(filePath, multiplyCallback);
+    }
+
+    public Integer fileReaderTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
         try (
-            BufferedReader reader  = new BufferedReader(new FileReader(filePath));
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
         ) {
-            Integer sum = 0;
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sum += Integer.parseInt(line);
-            }
-            return sum;
+            return callback.doSomethingWithReader(reader);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;

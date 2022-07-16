a5be2ef37ddd9852f487b47ebbbe64b37446f26a
fosong98 <fosong98@gmail.com>
2022-07-16 19:04:23 +0900

    <clean> : Remove repetitive patterns in calcSum, calcMultiply method
    * 템플릿 콜백을 이용하여 반복적인 패턴을 제거

--- app/src/main/java/springbook/user/test/learningtest/template/Calculator.java
+++ app/src/main/java/springbook/user/test/learningtest/template/Calculator.java
@@ -1,45 +1,32 @@
 package springbook.user.test.learningtest.template;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
     public int calcSum(String filePath) throws IOException {
-        BufferedReaderCallback sumCallback =
-                new BufferedReaderCallback() {
+        LineCallback sumCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader reader) throws IOException {
-                        Integer sum = 0;
-                        String line = null;
-                        while ((line = reader.readLine()) != null) {
-                            sum += Integer.parseInt(line);
-                        }
-                        return sum;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return Integer.valueOf(line) + value;
                     }
                 };
 
-        return fileReaderTemplate(filePath, sumCallback);
+        return lineReadTemplate(filePath, sumCallback, 0);
     }
 
     public int calcMultiply(String filePath) throws IOException {
-        BufferedReaderCallback multiplyCallback =
-                new BufferedReaderCallback() {
+        LineCallback multiplyCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader reader) throws IOException {
-                        int multiply = 1;
-                        String line = null;
-                        while ((line = reader.readLine()) != null) {
-                            multiply *= Integer.valueOf(line);
-                        }
-
-                        return multiply;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return Integer.valueOf(line) * value;
                     }
                 };
 
-        return fileReaderTemplate(filePath, multiplyCallback);
+        return lineReadTemplate(filePath, multiplyCallback, 1);
     }
 
     public Integer fileReaderTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
@@ -47,6 +34,22 @@ public class Calculator {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
         ) {
             return callback.doSomethingWithReader(reader);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+    public Integer lineReadTemplate(String filePath, LineCallback callback, int initVal) throws IOException {
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(filePath))
+        ) {
+            Integer res = initVal;
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;

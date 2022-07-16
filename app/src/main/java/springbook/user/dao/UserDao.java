815dceaf3be7e56098a026a03314821f82386560
fosong98 <fosong98@gmail.com>
2022-07-16 20:58:37 +0900

    <clean>: Substitute JdbcTemplate for JdbcContext
    * Spring에서 제공하는 템플릿/콜백 패턴을 적용하기 위해 JdbcTemplate로 교체.
    * deleteAll, add 메소드에 JdbcTempate의 update 메소드 적용

--- app/src/main/java/springbook/user/dao/UserDao.java
+++ app/src/main/java/springbook/user/dao/UserDao.java
@@ -1,6 +1,8 @@
 package springbook.user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -9,7 +11,7 @@ import java.sql.*;
 public class UserDao {
     public DataSource dataSource;
 
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public UserDao() {}
 
@@ -18,28 +20,15 @@ public class UserDao {
     }
 
     public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-
-        this.jdbcContext.setDataSource(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
 
         this.dataSource = dataSource;
     }
 
 
     public void add(final User user) throws SQLException {
-        jdbcContext.workWithStatementStrategy(
-            new StatementStrategy() {
-                @Override
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-
-                    return ps;
-                }
-            }
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword()
         );
     }
 
@@ -70,7 +59,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContext.executeSql("delete from users");
+        jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {

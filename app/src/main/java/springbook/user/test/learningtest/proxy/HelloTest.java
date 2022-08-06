53d5ff2a0165f2857756e2c2eb4a7c707a59c7cd
fosong98 <79358032+fosong98@users.noreply.github.com>
2022-08-06 23:10:18 +0900

    <test> : Make test for learning dynamic proxy
    * 다이내믹 프록시의 학습 테스트를 만들었다.

--- app/src/main/java/springbook/user/test/learningtest/proxy/HelloTest.java
+++ app/src/main/java/springbook/user/test/learningtest/proxy/HelloTest.java
@@ -2,6 +2,8 @@ package springbook.user.test.learningtest.proxy;
 
 import org.junit.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.junit.Assert.*;
 
 public class HelloTest {
@@ -14,8 +16,21 @@ public class HelloTest {
     }
 
     @Test
-    public void DecoratorProxy() {
+    public void decoratorProxy() {
         Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertEquals("HELLO TOBY", proxiedHello.sayHello("Toby"));
+        assertEquals("HI TOBY", proxiedHello.sayHi("Toby"));
+        assertEquals("THANK YOU TOBY", proxiedHello.sayThankYou("Toby"));
+    }
+
+    @Test
+    public void dynamicProxy() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] { Hello.class },
+                new UppercaseHandler(new HelloTarget())
+        );
+
         assertEquals("HELLO TOBY", proxiedHello.sayHello("Toby"));
         assertEquals("HI TOBY", proxiedHello.sayHi("Toby"));
         assertEquals("THANK YOU TOBY", proxiedHello.sayThankYou("Toby"));

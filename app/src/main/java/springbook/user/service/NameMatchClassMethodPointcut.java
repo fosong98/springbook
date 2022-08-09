742c257e96f3c0f41a1bbd258d7517dc9156290d
fosong98 <79358032+fosong98@users.noreply.github.com>
2022-08-09 14:05:54 +0900

    <feat> : Make pointcut including classFilter
    * ClassFilter가 포함된 pointcut 작성

---
+++ app/src/main/java/springbook/user/service/NameMatchClassMethodPointcut.java
@@ -0,0 +1,25 @@
+package springbook.user.service;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
+    @Override
+    public void setMappedName(String mappedName) {
+        this.setClassFilter(new SimpleClassFilter(mappedName));
+    }
+
+    static class SimpleClassFilter implements ClassFilter {
+        String mappedName;
+
+        public SimpleClassFilter(String mappedName) {
+            this.mappedName = mappedName;
+        }
+
+        @Override
+        public boolean matches(Class<?> clazz) {
+            return PatternMatchUtils.simpleMatch(mappedName, clazz.getSimpleName());
+        }
+    }
+}

5990baca2d307df5498421ded20de2411a29e93d
fosong98 <fosong98@gmail.com>
2022-08-17 13:13:09 +0900

    <test> : Apply upper transaction for the methods
    * 메소드를 하나의 트랜잭션으로 묶는다.

--- app/src/main/java/springbook/user/test/UserServiceTest.java
+++ app/src/main/java/springbook/user/test/UserServiceTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import springbook.user.dao.MockUserDao;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
@@ -160,10 +162,15 @@ public class UserServiceTest {
 
     @Test
     public void transactionSync() {
+        DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
+        TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
+
         userService.deleteAll();
 
         userService.add(users.get(0));
         userService.add(users.get(1));
+
+        transactionManager.commit(txStatus);
     }
 
     @Test

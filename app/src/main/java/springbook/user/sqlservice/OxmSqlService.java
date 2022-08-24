c46f0e215fbb282bfad41d91a21f5b90510fdff2
fosong98 <fosong98@gmail.com>
2022-08-24 17:54:16 +0900

    <clean> : Entrust SqlService method to the base
    * BaseSqlService로 중복 코드의 기능 구현을 넘기고 Oxm에서는 위임받아 사용

--- app/src/main/java/springbook/user/sqlservice/OxmSqlService.java
+++ app/src/main/java/springbook/user/sqlservice/OxmSqlService.java
@@ -12,21 +12,21 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
 public class OxmSqlService implements SqlService {
+    private final BaseSqlService baseSqlService = new BaseSqlService();
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
     private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
 
     @PostConstruct
     public void loadSql() {
-        this.oxmSqlReader.read(sqlRegistry);
+        this.baseSqlService.setSqlReader(this.oxmSqlReader);
+        this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+
+        this.baseSqlService.loadSql();
     }
 
     @Override
     public String getSql(String key) throws SqlRetrievalFailureException {
-        try {
-            return this.sqlRegistry.findSql(key);
-        } catch (SqlNotFoundException e) {
-            throw new SqlRetrievalFailureException(e);
-        }
+        return this.baseSqlService.getSql(key);
     }
 
     private class OxmSqlReader implements SqlReader {

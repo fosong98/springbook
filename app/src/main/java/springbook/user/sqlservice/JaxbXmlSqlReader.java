eb1c91ea91b40fdf0c208e3155df994f8c32ccc2
fosong98 <fosong98@gmail.com>
2022-08-24 11:46:02 +0900

    <feat> : Embody SqlReader and SqlRegistry
    * SqlReader를 JAXB로 구현
    * SqlRegistry를 Map으로 구현

---
+++ app/src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -0,0 +1,34 @@
+package springbook.user.sqlservice;
+
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class JaxbXmlSqlReader implements SqlReader {
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+        String contextPath = Sqlmap.class.getPackage().getName();
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream input = ClassLoader.getSystemResourceAsStream(sqlmapFile);
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(input);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

3fd42007b19f8ca6e03251c053028123b6a770df
fosong98 <fosong98@gmail.com>
2022-08-26 17:21:33 +0900

    <test> : Make EmbeddedDbSqlRegistryTest
    * 이전 작성한 ConcurrentHashMapSqlRegistryTest를 추상화한 클래스 작성
    * 추상화 테스트 클래스를 토대로 EmbeddedDbSqlRegistryTest 작성

--- app/src/main/java/springbook/user/test/ConcurrentHashMapSqlRegistryTest.java
+++ app/src/main/java/springbook/user/test/ConcurrentHashMapSqlRegistryTest.java
@@ -13,51 +13,9 @@ import java.util.Map;
 
 import static org.junit.Assert.*;
 
-public class ConcurrentHashMapSqlRegistryTest {
-    UpdatableSqlRegistry sqlRegistry;
-
-    @Before
-    public void setUp() {
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
-        sqlRegistry.registerSql("KEY1", "SQL1");
-        sqlRegistry.registerSql("KEY2", "SQL2");
-        sqlRegistry.registerSql("KEY3", "SQL3");
-    }
-
-    @Test
-    public void find() {
-        checkFindResult("SQL1", "SQL2", "SQL3");
-    }
-
-    private void checkFindResult(String expected1, String expected2, String expected3) {
-        assertEquals(expected1, sqlRegistry.findSql("KEY1"));
-        assertEquals(expected2, sqlRegistry.findSql("KEY2"));
-        assertEquals(expected3, sqlRegistry.findSql("KEY3"));
-    }
-
-    @Test(expected = SqlNotFoundException.class)
-    public void unknownKey() {
-        sqlRegistry.findSql("SQL9999!@#$");
-    }
-
-    @Test
-    public void updateSingle() {
-        sqlRegistry.updateSql("KEY2", "Modified2");
-        checkFindResult("SQL1", "Modified2", "SQL3");
-    }
-
-    @Test
-    public void updateMulti() {
-        Map<String, String> sqlmap = new HashMap<>();
-        sqlmap.put("KEY1", "Modified1");
-        sqlmap.put("KEY3", "Modified3");
-
-        sqlRegistry.updateSql(sqlmap);
-        checkFindResult("Modified1", "SQL2", "Modified3");
-    }
-
-    @Test(expected = SqlUpdateFailureException.class)
-    public void updateWithNotExistingKey() {
-        sqlRegistry.updateSql("SQL9999!@#$", "Modified2");
+public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        return new ConcurrentHashMapSqlRegistry();
     }
 }
---
+++ app/src/main/java/springbook/user/test/EmbeddedDbSqlRegistryTest.java
@@ -0,0 +1,30 @@
+package springbook.user.test;
+
+import org.junit.After;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import springbook.issuetracker.updatable.EmbeddedDbSqlRegistry;
+import springbook.user.sqlservice.UpdatableSqlRegistry;
+
+public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
+    EmbeddedDatabase db;
+
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        db = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("classpath:test/embeddeddb/schema.sql")
+                .build();
+
+        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(db);
+
+        return embeddedDbSqlRegistry;
+    }
+
+    @After
+    public void tearDown() {
+        db.shutdown();
+    }
+}

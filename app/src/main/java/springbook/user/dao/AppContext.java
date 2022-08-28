ba511766c5c05eab1fe2c57b217ef29631211438
fosong98 <fosong98@gmail.com>
2022-08-28 20:14:11 +0900

    <clean> : Extract properties
    * DB 연결정보를 담은 프로퍼티 파일 생성

--- app/src/main/java/springbook/user/dao/AppContext.java
+++ app/src/main/java/springbook/user/dao/AppContext.java
@@ -1,24 +1,16 @@
 package springbook.user.dao;
 
-import org.mariadb.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springbook.issuetracker.updatable.EmbeddedDbSqlRegistry;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
-import springbook.user.sqlservice.OxmSqlService;
-import springbook.user.sqlservice.SqlRegistry;
-import springbook.user.sqlservice.SqlService;
 import springbook.user.sqlservice.SqlServiceContext;
 import springbook.user.test.UserServiceTest;
 
@@ -28,18 +20,27 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
-@Import({SqlServiceContext.class, AppContext.TestAppContext.class, AppContext.ProductionAppContext.class})
+@Import(SqlServiceContext.class)
+@PropertySource("database.properties")
 public class AppContext {
     @Autowired
+    Environment env;
+
+    @Autowired
     UserDao userDao;
+
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass(Driver.class);
-        dataSource.setUrl(UserDaoPrivateData.address);
-        dataSource.setUsername(UserDaoPrivateData.name);
-        dataSource.setPassword(UserDaoPrivateData.password);
+        try {
+            dataSource.setDriverClass((Class<? extends java.sql.Driver>)
+                    Class.forName(env.getProperty("db.driverClass")));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
 
         return dataSource;
     }
